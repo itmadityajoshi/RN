ba2927e16d8966c8b61556c2985349416d68f974
unknown <adityajoshi316@gmail.com>
2026-07-09 14:31:02 +0200

    functoin related study finished

--- function.js
+++ function.js
@@ -42,3 +42,37 @@ console.log(result);
     return sends a value back to where the function was called.
 */
 
+// Lesson 5 
+/*  Arrow function are a shorter way to write a functions.
+They are used everywhere in React and React-Native. */
+
+const greetName = (name) => {
+    return ("Hello " + name + ".")
+};
+
+console.log(greetName("pukar"));
+
+// if there is only one line, you can omit the {} and return: 
+const greeFirst = (name) => "Hello " + name;
+console.log(greeFirst("Hari"));
+
+
+/* Objects are one of the most important topics in Javascripts. In React native, you'll use them constatnlty for propos,state, API responses, navigation parameters, and more. */
+
+// what is an object?
+//  Aan object stores data in key value pairs.
+
+const student ={
+    nam : "Mausam",
+    age: 25,
+    country : "Nepal",
+}
+/* Here nam,age, country are keys and "Mausam","Nepal", 25 are keys */
+
+
+// Accessing an object Properties
+console.log(student.nam);
+console.log(student.age);
+
+student.University = "JKU"
+console.log(student);
