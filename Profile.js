8d8f3af13261d832e6b865bbbccc8072aaf4bd69
unknown <adityajoshi316@gmail.com>
2026-07-11 22:33:19 +0200

    react component created

---
+++ Profile.js
@@ -0,0 +1,14 @@
+function Profile(){
+    const firstName = "Mausam";
+    const lastName = "Chaudhary";
+    return(
+        <div>
+            <h1> {firstName} {lastName} </h1>
+            <p>
+                {10+15}
+            </p>
+        </div>
+    );
+}
+
+export default Profile;
