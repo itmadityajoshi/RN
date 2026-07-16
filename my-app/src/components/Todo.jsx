6b7d959aeb22590f4d6c32ae21542109c81cd165
unknown <adityajoshi316@gmail.com>
2026-07-16 19:29:50 +0200

    todo don

--- my-app/src/components/Todo.jsx
+++ my-app/src/components/Todo.jsx
@@ -4,13 +4,14 @@ import { useState } from "react";
 const Todo = () => {
   const [task, setTask] = useState("");
   const [todos, setTodo] = useState([]);
+  const [editingIndex, seteditingIndex] = useState(null)
 
   function addTodo() {
     if (task === "") {
       return;
     }
     setTodo([...todos, task]);
-    console.log(task);
+    // console.log(task);
     setTask("");
   }
 
@@ -21,16 +22,40 @@ const Todo = () => {
     setTodo(newTodos);
   }
 
+
+
+  function edit(index){
+    seteditingIndex(index)
+    setTask(todos[index])
+  }
+
+  function updateTodo(){
+    const updateTodos = todos.map((todos, currentIndex)=>{
+      if (editingIndex == currentIndex){
+        return task
+      }
+      return todo;
+    })
+    setTodo(updateTodos);
+    seteditingIndex(null)
+    setTask("")
+  }
+
   return (
     <>
       <input value={task} onChange={(e) => setTask(e.target.value)} />
       <b> </b>
-      <button onClick={addTodo}>Add Todo</button>
+      <button onClick={editingIndex === null ? addTodo : updateTodo}>
+        {
+          editingIndex === null ? "Add Todo" : "Update Todo"
+        }
+      </button>
 
       {todos.map((todo, index) => (
         <>
           <h2 key={index}>{todo} </h2>{" "}
-          <button onClick={() => remove(index)}>remove Todo</button>
+          <button onClick={() => remove(index)}>Delete</button> <b>  </b>
+          <button onClick={() => edit(index)}>Edit</button>
         </>
       ))}
     </>
